4fb0315af006f68c0c2e3dca5f52f3f7305539c8
rahulk <Rahul.k.mbox1@gmail.com>
2018-11-26 19:24:14 -0500

    working

---
+++ clicky-game/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="bottom">
+      Clicky Game! <img alt="react" src="assets/images/friends2.gif" />
+    </div>
+  </footer>
+);
+
+export default Footer;
